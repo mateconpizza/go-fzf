09b7cb5c7e6c684d74b9ee00fc68e8a72272559d
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2026-07-31 18:02:58 -0300

    test(menu): add example test for menu selection

---
+++ menu_test.go
@@ -0,0 +1,47 @@
+package menu_test
+
+import (
+	"fmt"
+
+	menu "github.com/mateconpizza/go-fzf"
+)
+
+type Item struct {
+	Name     string
+	Quantity int
+}
+
+func (item Item) String() string {
+	return fmt.Sprintf("%-12s (%d)", item.Name, item.Quantity)
+}
+
+var items = []Item{
+	{Name: "Apples", Quantity: 12},
+	{Name: "Bananas", Quantity: 8},
+	{Name: "Oranges", Quantity: 5},
+	{Name: "Pears", Quantity: 3},
+	{Name: "Palandri", Quantity: 1},
+}
+
+func ExampleMenu() {
+	m := menu.New[Item](
+		menu.WithDefaults(true), // uses `$FZF_DEFAULT_OPTS_FILE` and `$FZF_DEFAULT_OPTS`
+		menu.WithPrompt("Select> "),
+		menu.WithHeader("Inventory"),
+		menu.WithBorder(menu.BorderRounded),
+		menu.WithBorderLabel("= demo ="),
+		menu.WithPreviewCmd("echo {1}"),
+		menu.WithHeight("40%"),
+		menu.WithFooter("~~ Give me apples ~~"),
+	)
+
+	selected, err := m.Select(items)
+	if err != nil {
+		return
+	}
+
+	for _, item := range selected {
+		fmt.Println(item.Name)
+		// Output: Apples
+	}
+}
